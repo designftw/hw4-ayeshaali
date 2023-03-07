4e9011417960e5a59bd3819b9630187b0cd08347
ayeshaali <aliayeshanyc@gmail.com>
2023-03-06 22:39:02 -0500

    exercise 2

--- css-demo/index.js
+++ css-demo/index.js
@@ -11,29 +11,67 @@ globalThis.app = createApp({
         position: "outside",
         customSymbol: "alpha",
         customSystem: "cyclic",
-        suffix: "none"
+        suffix: "period"
 	},
 	computed: {
 		css () {
-            console.log(this.type, this.position, this.imageIndex, this.customSymbols, this.customSystem)
+            console.log(this.type, this.position, this.imageIndex, this.customSymbol, this.customSystem, this.suffix)
 			let css = "list-style: " +  this.position + " ";
             css += (this.type === "custom") ? this.customSymbol+"-"+this.customSystem : this.type
-            css += (this.suffix === "none") ? "" : "-"+this.suffix
+            css += (this.suffix === "period") ? "" : "-"+this.suffix
             if (this.imageIndex == 1 || this.imageIndex == 2) {
                 css += " url("+ this.images[this.imageIndex] + ")";
             } else if (this.imageIndex == 3) {
                 css += " linear-gradient(to left bottom, red, blue)"
             }
-			return css;
+			return css; 
 		},
-        disabled() {return this.type!=="custom"}
+        
+        disabled() {return this.type!=="custom"},
+
+        counterStyle() {
+            let css = ""
+            if (this.type !== "custom") {
+                return ""
+            } else {
+                let name = this.customSymbol+"-"+this.customSystem;
+                name+=(this.suffix === "period") ? "" : "-"+this.suffix;
+                let symbol = ""
+                switch(this.customSymbol) {
+                    case "alpha":
+                        symbol= "A B C D";
+                        break;
+                    case "thumbs":
+                        symbol= '"\\1F44D"';
+                        break;
+                    case "circled-alpha":
+                        symbol= "Ⓐ Ⓑ Ⓒ Ⓓ Ⓔ Ⓕ Ⓖ Ⓗ Ⓘ Ⓙ Ⓚ Ⓛ Ⓜ Ⓝ Ⓞ Ⓟ Ⓠ Ⓡ Ⓢ Ⓣ Ⓤ Ⓥ Ⓦ Ⓧ Ⓨ Ⓩ";
+                        break;
+                }
+
+                let suffix = ""
+                switch(this.suffix) {
+                    case "period":
+                        suffix = "}"
+                        break;
+                    case "paren":
+                        suffix=  'suffix: ") ";}';
+                        break;
+                    case "line":
+                        suffix= 'suffix: "| ";}';
+                        break;
+                }
+
+                css = "\n\n @counter-style "+name+" {\n\t system: "+this.customSystem+"; \n\tsymbols: "+symbol+";\n\t"+suffix;
+                return css
+            }
+        }
 	},
     methods: {
         copy() {
-          const element = this.$refs.code;
-          element.select();
-          element.setSelectionRange(0, 99999);
-          document.execCommand('copy');
+          const css = this.$refs.code1;
+          const counterStyle = this.$refs.code2;
+          navigator.clipboard.writeText(css.innerHTML + counterStyle.innerHTML)
         }
     },
 },  "#app");
